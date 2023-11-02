282161f0f95d545611ce2fee431ca9da54111154
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-11-02 13:38:39 +0800

    Update digiwoo_referral.php

--- digiwoo_referral.php
+++ digiwoo_referral.php
@@ -107,96 +107,103 @@ if ( in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get
         }
 
         // 1. Set the Referral ID in WooCommerce Session and Cookies
-        function set_ref_id_in_session() {
-            if (isset($_GET['_ref'])) {
-                $ref_id = sanitize_text_field($_GET['_ref']);
-                if (isset($_COOKIE[REF_COOKIE]) && $_COOKIE[REF_COOKIE] !== $ref_id) {
-                    setcookie(REF_COOKIE, '', time() - 3600, "/", "", is_ssl(), true);
-                }
+        function set_marketing_params_in_session() {
+            // List of all query parameters and their respective cookie names
+            $params = [
+                '_ref'        => REF_COOKIE,
+                'utm_source'  => UTM_SOURCE_COOKIE,
+                'utm_medium'  => UTM_MEDIUM_COOKIE,
+                'utm_term'    => UTM_TERM_COOKIE,
+                'utm_campaign'=> UTM_CAMPAIGN_COOKIE,
+                'utm_content' => UTM_CONTENT_COOKIE,
+                'cid'         => CID_COOKIE,
+                'lid'         => LID_COOKIE
+            ];
 
-                if (!isset($_COOKIE[REF_COOKIE])) {
-                    WC()->session->set('ref_id', $ref_id);                
-                    $cookie_duration = get_option('digiwoo_cookie_duration', 365);
-                    $cookie_expiry = time() + ($cookie_duration * 24 * 60 * 60); 
-                    setcookie(REF_COOKIE, $ref_id, $cookie_expiry, "/", "", is_ssl(), true);
+            foreach ($params as $param => $cookieName) {
+                if (isset($_GET[$param])) {
+                    $paramValue = sanitize_text_field($_GET[$param]);
+
+                    // If the cookie already exists and is different, reset it
+                    if (isset($_COOKIE[$cookieName]) && $_COOKIE[$cookieName] !== $paramValue) {
+                        setcookie($cookieName, '', time() - 3600, "/", "", is_ssl(), true);
+                    }
+
+                    // Set new cookie if it doesn't exist
+                    if (!isset($_COOKIE[$cookieName])) {
+                        $cookie_duration = get_option('digiwoo_cookie_duration', 365);
+                        $cookie_expiry = time() + ($cookie_duration * 24 * 60 * 60); 
+                        setcookie($cookieName, $paramValue, $cookie_expiry, "/", "", is_ssl(), true);
+                    }
                 }
             }
         }
-        add_action('init', 'set_ref_id_in_session', 10);
+        add_action('init', 'set_marketing_params_in_session', 10);
 
-        // 1. Set the Referral ID in WooCommerce Session and Cookies
-        function set_lid_id_in_session() {
-            if (isset($_GET['lid'])) {
-                $lid_id = sanitize_text_field($_GET['lid']);
-                if (isset($_COOKIE[LID_COOKIE]) && $_COOKIE[LID_COOKIE] !== $lid_id) {
-                    setcookie(LID_COOKIE, '', time() - 3600, "/", "", is_ssl(), true);
-                }
-
-                if (!isset($_COOKIE[LID_COOKIE])) {
-                    WC()->session->set('lid_id', $lid_id);                
-                    $cookie_duration = get_option('digiwoo_cookie_duration', 365);
-                    $cookie_expiry = time() + ($cookie_duration * 24 * 60 * 60); 
-                    setcookie(LID_COOKIE, $lid_id, $cookie_expiry, "/", "", is_ssl(), true);
-                }
-            }
-        }
-        add_action('init', 'set_lid_id_in_session', 10);
 
         // 2. Capture the Referral ID from the URL
-        function get_referral_id_from_url() {
-            if (isset($_COOKIE[REF_COOKIE]) && !empty($_COOKIE[REF_COOKIE])) {
-                return sanitize_text_field( $_COOKIE[REF_COOKIE] ); 
-            } elseif( isset($_GET['_ref']) && !empty($_GET['_ref'])) {
-                return sanitize_text_field( $_GET['_ref'] );
+        function get_marketing_params() {
+            $params = [
+                'ref'          => REF_COOKIE,
+                'utm_source'   => UTM_SOURCE_COOKIE,
+                'utm_medium'   => UTM_MEDIUM_COOKIE,
+                'utm_term'     => UTM_TERM_COOKIE,
+                'utm_campaign' => UTM_CAMPAIGN_COOKIE,
+                'utm_content'  => UTM_CONTENT_COOKIE,
+                'cid'          => CID_COOKIE,
+                'lid'          => LID_COOKIE
+            ];
+
+            $values = [];
+
+            foreach ($params as $urlParam => $cookieName) {
+                // Check if the value is in the cookie first
+                if (isset($_COOKIE[$cookieName]) && !empty($_COOKIE[$cookieName])) {
+                    $values[$urlParam] = sanitize_text_field($_COOKIE[$cookieName]);
+                }
+                // If not in cookie, check the URL parameter
+                elseif (isset($_GET[$urlParam]) && !empty($_GET[$urlParam])) {
+                    $values[$urlParam] = sanitize_text_field($_GET[$urlParam]);
+                } else {
+                    $values[$urlParam] = '';
+                }
             }
-            return '';
+
+            return $values;
         }
 
-        // 2. Capture the Referral ID from the URL
-        function get_referral_lid_from_url() {
-            if (isset($_COOKIE[LID_COOKIE]) && !empty($_COOKIE[LID_COOKIE])) {
-                return sanitize_text_field( $_COOKIE[LID_COOKIE] ); 
-            } elseif( isset($_GET['lid']) && !empty($_GET['lid'])) {
-                return sanitize_text_field( $_GET['lid'] );
-            }
-            return '';
-        }
 
         // 3. Add Hidden Field to WooCommerce Checkout
-        function add_hidden_referral_field_to_checkout( $checkout ) {
-            $referral_id = get_referral_id_from_url();
-            $referral_lid = get_referral_lid_from_url();
+        function add_hidden_marketing_fields_to_checkout( $checkout ) {
+            $marketing_params = get_marketing_params();
 
-            // Add a hidden field to the checkout
-            woocommerce_form_field( '_ref', array(
-                'type'          => 'hidden',
-                'class'         => array('referral-id-hidden-field'),
-                'label_class'   => array('hidden'),
-                'input_class'   => array('hidden'),
-            ), $referral_id );
-
-            // Add a hidden field to the checkout
-            woocommerce_form_field( '_lid', array(
-                'type'          => 'hidden',
-                'class'         => array('referral-lid-hidden-field'),
-                'label_class'   => array('hidden'),
-                'input_class'   => array('hidden'),
-            ), $referral_lid );
+            // Loop through all marketing parameters and add them as hidden fields
+            foreach ($marketing_params as $param => $value) {
+                woocommerce_form_field( $param, array(
+                    'type'          => 'hidden',
+                    'class'         => array($param . '-hidden-field'),
+                    'label_class'   => array('hidden'),
+                    'input_class'   => array('hidden'),
+                ), $value );
+            }
         }
-        add_action('woocommerce_after_checkout_billing_form', 'add_hidden_referral_field_to_checkout');
+
+        add_action('woocommerce_after_checkout_billing_form', 'add_hidden_marketing_fields_to_checkout');
+
 
         // 4. Save Referral ID as Order Meta
-        function save_referral_id_in_order_meta( $order_id ) {
-            if( !empty($_POST['_ref']) ) {
-                $ref_id = $_POST['_ref'];
-                update_post_meta( $order_id, 'referral_id_order', $ref_id );
-            }
-            if( !empty($_POST['_lid']) ) {
-                $lid_id = $_POST['_lid'];
-                update_post_meta( $order_id, 'referral_lid_order', $lid_id );
+        function save_marketing_params_in_order_meta( $order_id ) {
+            $marketing_params = get_marketing_params();
+
+            foreach ($marketing_params as $param => $value) {
+                if (!empty($value)) {
+                    update_post_meta($order_id, $param . '_order', $value);
+                }
             }
         }
-        add_action('woocommerce_checkout_update_order_meta', 'save_referral_id_in_order_meta');
+
+        add_action('woocommerce_checkout_update_order_meta', 'save_marketing_params_in_order_meta');
+
 
     }
 
